06d8ef11a89018ee5a055e2229f38d878e84c8ec
JordyJord <jordynwinnie@gmail.com>
2020-06-14 16:11:19 +0800

    Project Reorganised, adding bounding box

---
+++ Assets/Scripts/BoundingBoxScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingBoxScript : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
